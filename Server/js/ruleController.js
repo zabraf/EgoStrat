d4efad43afec1038d82f817b13919f5bae0b4e79
gaelMRT <gael.mrt@eduge.ch>
2019-10-03 08:27:39 +0200

    Added enum to ruleController

--- Server/js/ruleController.js
+++ Server/js/ruleController.js
@@ -1,3 +1,22 @@
+const color = {
+    BLUE : '',
+    RED : '',
+    SPECTATOR : ''
+}
+const type = {
+    BOMB : '' ,
+    CAPTAIN : '' ,
+    COLONEL : '',
+    FLAG : '',
+    GENERAL : '' ,
+    LIEUTENANT : '',
+    MAJOR : '',
+    MARSHAL : '' ,
+    MINER : '' ,
+    SCOUT : '' ,
+    SERGEANT : '' ,
+    SPY : ''
+}
 class RuleController{
-    
+
 }
