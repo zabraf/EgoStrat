d3125baa0146d0123a369987b91833897a892aea
zabraf <Administrateur@R104PC11>
2019-10-03 08:57:02 +0200

    Changed server Setting so it output index.html

--- Server/Server.js
+++ Server/Server.js
@@ -1,15 +1,19 @@
-const http = require('http');
+let http = require('http');
 let fs = require('fs');
-
-const hostname = '10.5.51.45';
-const port = 80;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+ 
+let handleRequest = (request, response) => {
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+     fs.readFile('./Client/index.html', null, function (error, data) {
+        if (error) {
+            response.writeHead(404);
+            response.write('Whoops! File not found!');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+};
+ 
+http.createServer(handleRequest).listen(80);
