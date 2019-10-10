5895593fb69842f3d64ea671b0f047fb3f3aaad6
zabraf <Administrateur@R104PC11>
2019-10-10 09:32:58 +0200

    Added Implemtation of piece.js / player.js / tile.js

--- Server/js/player.js
+++ Server/js/player.js
@@ -1,7 +1,32 @@
-class Player{
-    constructor(anUsername,aColor){
-        this.isRacist = Math.floor(Math.random()*1000) == 1;
-        this.color = aColor;
-        this.username = anUsername;
-    }
+class Player {
+        constructor(anUsername, aColor, ruleControlleur) {
+                this.isRacist = Math.floor(Math.random() * 1000) == 1;
+                this.color = aColor;
+                this.username = anUsername;
+                this.ruleControlleur = ruleControlleur;
+        }
+        GetUsername(username) {
+                this.username = username;
+        }
+        GetUsername() {
+                return this.username;
+        }
+        async PutPieces(PiecesToPut, TileCanHere) {
+              PiecesToPut.forEach(element => {
+                      do
+                      {
+                        randomX = Math.floor(Math.random() * TileCanHere.length)
+                        randomY = Math.floor(Math.random() * TileCanHere[0].length)
+                        if(TileCanHere[randomX,randomY] == null)
+                        {
+                                TileCanHere[randomX,randomY].SetPieces(element);
+                                element = null;
+                        }
+                      }while (element != null)
+              });
+                resolve(true);
+        }
+        GetColor() {
+                return this.color;
+        }
 }
