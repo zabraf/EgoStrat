4a4af202f42a55d40cf18dc5de249e9a52de0d06
Pyroblastouille <pyroblastouille@pop-os.localdomain>
2019-10-17 09:18:41 +0200

    Added x and y to Tile

--- Server/js/tile.js
+++ Server/js/tile.js
@@ -1,24 +1,38 @@
 class Tile{
-    constructor(canGo = true,aPiece = null){
+    constructor(anX,anY,canGo = true,aPiece = null){
         this.canGo = canGo;
         this.piece = aPiece;
+        this.x = anX;
+        this.y = anY;
+        this.selected = false;
     }
     GetPiece() {
         return this.piece;
+    }
+    GetX() {
+        return this.x;
+    }
+    GetY() {
+        return this.y;
     }
     SetPiece(piece) {
         this.piece = piece;
     }
     DrawTile() {
         var draw = "";
-        if (canGo) {
-            draw = '<div class="tile-go">';
-            if (piece != null) {
+        if (this.canGo) {
+            if(this.selected){
+                draw = '<div class="tile go selected">';
+            }else{
+                draw = '<div class="tile go">';
+            }
+           
+            if (this.piece != null) {
                 draw += this.piece.draw;
             }
         }
         else {
-            draw = '<div class="tile-cantgo">';
+            draw = '<div class="tile cantgo">';
         }
         draw += '</div>';
         return draw;
