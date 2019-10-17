66f90b972428ab13b77ef23f722884bdb2a702c2
zabraf <Administrateur@R104PC11>
2019-10-17 10:45:10 +0200

    implemented WhereTileCanGo and Unselected

--- Server/js/ruleController.js
+++ Server/js/ruleController.js
@@ -1,39 +1,39 @@
 const color = {
-    BLUE : 'blue',
-    RED : 'red',
-    SPECTATOR : 'grey'
+    BLUE: 'blue',
+    RED: 'red',
+    SPECTATOR: 'grey'
 }
 const type = {
-    MARSHAL     : {name:'MARSHAL',img : '/img/marshal.svg', strength : 10} ,
-    GENERAL     : {name:'GENERAL',img : '/img/general.svg', strength : 9} ,
-    COLONEL     : {name:'COLONEL',img : '/img/colonel.svg', strength : 8},
-    MAJOR       : {name:'MAJOR',img : '/img/major.svg', strength : 7},
-    CAPTAIN     : {name:'CAPTAIN',img : '/img/captain.svg', strength : 6} ,
-    LIEUTENANT  : {name:'LIEUTENANT',img : '/img/lieutenant.svg', strength : 5},
-    SERGEANT    : {name:'SERGEANT',img : '/img/sergeant.svg', strength : 4} ,
-    MINER       : {name:'MINER',img : '/img/miner.svg', strength : 3} ,
-    SCOUT       : {name:'SCOUT',img : '/img/scout.svg', strength : 2} ,
-    SPY         : {name:'SPY',img : '/img/spy.svg', strength : 1},
-    FLAG        : {name:'FLAG',img : '/img/flag.svg', strength : 0},
-    BOMB        : {name:'BOMB',img : '/img/bomb.svg',strength : 11} ,
+    MARSHAL: { name: 'MARSHAL', img: '/img/marshal.svg', strength: 10 },
+    GENERAL: { name: 'GENERAL', img: '/img/general.svg', strength: 9 },
+    COLONEL: { name: 'COLONEL', img: '/img/colonel.svg', strength: 8 },
+    MAJOR: { name: 'MAJOR', img: '/img/major.svg', strength: 7 },
+    CAPTAIN: { name: 'CAPTAIN', img: '/img/captain.svg', strength: 6 },
+    LIEUTENANT: { name: 'LIEUTENANT', img: '/img/lieutenant.svg', strength: 5 },
+    SERGEANT: { name: 'SERGEANT', img: '/img/sergeant.svg', strength: 4 },
+    MINER: { name: 'MINER', img: '/img/miner.svg', strength: 3 },
+    SCOUT: { name: 'SCOUT', img: '/img/scout.svg', strength: 2 },
+    SPY: { name: 'SPY', img: '/img/spy.svg', strength: 1 },
+    FLAG: { name: 'FLAG', img: '/img/flag.svg', strength: 0 },
+    BOMB: { name: 'BOMB', img: '/img/bomb.svg', strength: 11 },
 }
 
 const DEFAULT_SET = {
-    'MARSHAL'    : 1 ,
-    'GENERAL'    : 1 ,
-    'COLONEL'    : 2 ,
-    'MAJOR'      : 3 ,
-    'CAPTAIN'    : 4 ,
-    'LIEUTENANT' : 4 ,
-    'SERGEANT'   : 4 ,
-    'MINER'      : 5 ,
-    'SCOUT'      : 8 ,
-    'SPY'        : 1 ,
-    'FLAG'       : 1 ,
-    'BOMB'       : 6 ,
+    'MARSHAL': 1,
+    'GENERAL': 1,
+    'COLONEL': 2,
+    'MAJOR': 3,
+    'CAPTAIN': 4,
+    'LIEUTENANT': 4,
+    'SERGEANT': 4,
+    'MINER': 5,
+    'SCOUT': 8,
+    'SPY': 1,
+    'FLAG': 1,
+    'BOMB': 6,
 }
 
-class RuleController{
+class RuleController {
 
     /**
      * 
@@ -43,23 +43,23 @@ class RuleController{
      * @param {number} Height 
      * @param {Array<GameSet>} aListOfPieces 'PieceName' : nbPieces 
      */
-    constructor(aPlayer1Username = "P1",aPlayer2Username = "P2",Width = 10,Height = 10,aListOfPieces = DEFAULT_SET){
+    constructor(aPlayer1Username = "P1", aPlayer2Username = "P2", Width = 10, Height = 10, aListOfPieces = DEFAULT_SET) {
         movablePieces = aListOfPieces['CAPTAIN'] +
-        aListOfPieces['COLONEL'] +
-        aListOfPieces['GENERAL'] +
-        aListOfPieces['LIEUTENANT'] +
-        aListOfPieces['MAJOR'] +
-        aListOfPieces['MARSHAL'] +
-        aListOfPieces['MINER'] +
-        aListOfPieces['SCOUT'] +
-        aListOfPieces['SERGEANT'] +
-        aListOfPieces['SPY'];
+            aListOfPieces['COLONEL'] +
+            aListOfPieces['GENERAL'] +
+            aListOfPieces['LIEUTENANT'] +
+            aListOfPieces['MAJOR'] +
+            aListOfPieces['MARSHAL'] +
+            aListOfPieces['MINER'] +
+            aListOfPieces['SCOUT'] +
+            aListOfPieces['SERGEANT'] +
+            aListOfPieces['SPY'];
 
-        if(aPlayer1Username == aPlayer2Username){
+        if (aPlayer1Username == aPlayer2Username) {
             throw new Exception("Cannot have 2 same username");
         }
-        this.player1 = new Player(aPlayer1Username,color.RED,movablePieces);
-        this.player2 = new Player(aPlayer2Username,color.BLUE,movablePieces);
+        this.player1 = new Player(aPlayer1Username, color.RED, movablePieces);
+        this.player2 = new Player(aPlayer2Username, color.BLUE, movablePieces);
 
         this.map = [];
 
@@ -69,7 +69,7 @@ class RuleController{
         var HeightDivided = 100 / Height
         for (var i = 0; i < Width; i++) {
             for (var j = 0; j < Height; j++) {
-                if (WidthDivided * i == 20 || WidthDivided * i == 30 || WidthDivided * i == 60 || WidthDivided * i == 70 &&HeightDivided * j == 40 || HeightDividedgit * j == 50) {
+                if (WidthDivided * i == 20 || WidthDivided * i == 30 || WidthDivided * i == 60 || WidthDivided * i == 70 && HeightDivided * j == 40 || HeightDividedgit * j == 50) {
                     map[i][j] = new tile(false);
                 }
                 map[i][j] = new tile();
@@ -79,7 +79,7 @@ class RuleController{
     }
     MovePiece(tileDeparture, tileArrival) {
         var result = false;
-        
+
 
 
         return result;
@@ -146,17 +146,17 @@ class RuleController{
 
         return remainingOpponent <= 0 || this.piecesPlayer2['FLAG'] == 0;
     }
-    CheckWinP2(){
+    CheckWinP2() {
         var remainingOpponent = this.piecesPlayer2['CAPTAIN'] +
-        this.piecesPlayer2['COLONEL'] +
-        this.piecesPlayer2['GENERAL'] +
-        this.piecesPlayer2['LIEUTENANT'] +
-        this.piecesPlayer2['MAJOR'] +
-        this.piecesPlayer2['MARSHAL'] +
-        this.piecesPlayer2['MINER'] +
-        this.piecesPlayer2['SCOUT'] +
-        this.piecesPlayer2['SERGEANT'] +
-        this.piecesPlayer2['SPY'];
+            this.piecesPlayer2['COLONEL'] +
+            this.piecesPlayer2['GENERAL'] +
+            this.piecesPlayer2['LIEUTENANT'] +
+            this.piecesPlayer2['MAJOR'] +
+            this.piecesPlayer2['MARSHAL'] +
+            this.piecesPlayer2['MINER'] +
+            this.piecesPlayer2['SCOUT'] +
+            this.piecesPlayer2['SERGEANT'] +
+            this.piecesPlayer2['SPY'];
 
         return remainingOpponent <= 0 || this.piecesPlayer1['FLAG'] == 0;
     }
@@ -169,11 +169,72 @@ class RuleController{
         }
 
     }
-    CheckPieceCanGo(tileDeparture,tileArrival) {
-        var result = false;
+    WherePieceCanGo(tileDeparture) {
+        var canGoTopY = false;
+        var canGoBotY = false;
+        var canGoRightX = false;
+        var canGoLeftX = false;
+        if (tileDeparture.piece.GetType() == type.SCOUT) {
+            canGoTopY = true;
+            canGoBotY = true;
+            canGoRightX = true;
+            canGoLeftX = true;
+        }
 
-        return result;
+        var X = tileDeparture.GetX();
+        var Y = tileDeparture.GetY() + 1;
+        do {
+            if (this.map[X][Y] == null || this.map[X][Y].getPiece() != null || this.map[X][Y].canGo == false) {
+                canGoTopY = false;
+            }
+            else {
+                this.map[X][Y].selected = true;
+                Y = Y + 1;
+            }
+
+        } while (canGoTopY)
+
+        X = tileDeparture.GetX();
+        Y = tileDeparture.GetY() - 1;
+        do {
+            if (this.map[X][Y] == null || this.map[X][Y].getPiece() != null || this.map[X][Y].canGo == false) {
+                canGoBotY = false;
+            }
+            else {
+                this.map[X][Y].selected = true;
+                Y = Y - 1;
+            }
+        } while (canGoBotY)
+
+        X = tileDeparture.GetX() + 1;
+        Y = tileDeparture.GetY();
+        do {
+            if (this.map[X][Y] == null || this.map[X][Y].getPiece() != null || this.map[X][Y].canGo == false) {
+                canGoRightX = false;
+            }
+            else {
+                this.map[X][Y].selected = true;
+                X = X + 1;
+            }
+        } while (canGoRightX)
+
+        X = tileDeparture.GetX() - 1;
+        Y = tileDeparture.GetY();
+        do {
+            if (this.map[X][Y] == null || this.map[X][Y].getPiece() != null || this.map[X][Y].canGo == false) {
+                canGoLeftX = false;
+            }
+            else {
+                this.map[X][Y].selected = true;
+                X = X - 1;
+            }
+        } while (canGoLeftX)
     }
-
-
+    Unselect()
+    {
+        map.forEach(mapx => {
+            mapx.forEach(tile => {tile.selected = false;});
+            
+        });
+    }
 }
