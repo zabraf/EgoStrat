7ce25514ba43b9b01e338ccc722094afd4df2d04
Pyroblastouille <pyroblastouille@pop-os.localdomain>
2019-10-17 09:13:47 +0200

    Re-put implementation of player,piece and tile (Sorry)

--- Server/js/player.js
+++ Server/js/player.js
@@ -1,9 +1,34 @@
-class Player{
-    constructor(anUsername,aColor,aNbMovablePieces){
-        this.isRacist = Math.floor(Math.random()*1000) == 1;
-        this.color = aColor;
-        this.username = anUsername;
-        this.flagAlive = true;
-        this.nbMovablePiece = aNbMovablePieces;
+class Player {
+        constructor(anUsername, aColor, ruleControlleur,anNbMovablePieces) {
+                this.isRacist = Math.floor(Math.random() * 1000) == 1;
+                this.color = aColor;
+                this.username = anUsername;
+                this.isAlive = true;
+                this.nbMovablePieces = anNbMovablePieces
+                this.ruleControlleur = ruleControlleur;
+        }
+        GetUsername(username) {
+                this.username = username;
+        }
+        GetUsername() {
+                return this.username;
+        }
+        async PutPieces(PiecesToPut, TileCanHere) {
+              PiecesToPut.forEach(element => {
+                      do
+                      {
+                        randomX = Math.floor(Math.random() * TileCanHere.length)
+                        randomY = Math.floor(Math.random() * TileCanHere[0].length)
+                        if(TileCanHere[randomX,randomY] == null)
+                        {
+                                TileCanHere[randomX,randomY].SetPieces(element);
+                                element = null;
+                        }
+                      }while (element != null)
+              });
+                resolve(true);
+        }
+        GetColor() {
+                return this.color;
+        }
     }
-}
--- Server/js/tile.js
+++ Server/js/tile.js
@@ -3,4 +3,24 @@ class Tile{
         this.canGo = canGo;
         this.piece = aPiece;
     }
+    GetPiece() {
+        return this.piece;
+    }
+    SetPiece(piece) {
+        this.piece = piece;
+    }
+    DrawTile() {
+        var draw = "";
+        if (canGo) {
+            draw = '<div class="tile-go">';
+            if (piece != null) {
+                draw += this.piece.draw;
+            }
+        }
+        else {
+            draw = '<div class="tile-cantgo">';
+        }
+        draw += '</div>';
+        return draw;
+    }
 }
